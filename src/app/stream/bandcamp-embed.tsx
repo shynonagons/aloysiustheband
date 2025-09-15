dde104c20d4e172c1f1cc64780499f628e1134d8
John A Maloney <12534169+shynonagons@users.noreply.github.com>
2025-09-15 11:36:54 -0500

    Stream page

---
+++ src/app/stream/bandcamp-embed.tsx
@@ -0,0 +1,11 @@
+export const BandcampEmbed = () => (
+  <iframe
+    style={{ border: 0, width: '350px', height: '470px' }}
+    src="https://bandcamp.com/EmbeddedPlayer/album=3064969758/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/"
+    seamless
+  >
+    <a href="https://aloysius.bandcamp.com/album/mortise-tenon">
+      mortise+tenon by aloysius the band
+    </a>
+  </iframe>
+);
