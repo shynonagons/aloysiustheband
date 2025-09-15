293a18a2c23559db27d51f4b7335b1f3b2a44462
John A Maloney <12534169+shynonagons@users.noreply.github.com>
2025-09-15 13:30:45 -0500

    Add early work review

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,9 +1,7 @@
 import InstagramIcon from '@/icons/instagram';
 import BandcampIcon from '@/icons/bandcamp';
 import YoutubeIcon from '@/icons/youtube';
-import SubstackIcon from '@/icons/substack';
 import SpotifyIcon from '@/icons/spotify';
-import clsx from 'clsx';
 import ArrowUpRightFromSquareIcon from '@/icons/arrow-up-right-from-square';
 
 const DEFAULT_ICON = <ArrowUpRightFromSquareIcon className='w-8 h-8' />;
@@ -16,19 +14,29 @@ const socialLinks = [
     text: '🎟️ Tickets for MORTISE+TENON album release 10/10 Amsterdam Bar',
   },
   {
-    title: 'Bandcamp',
-    url: 'https://aloysius.bandcamp.com',
-    hoverColor: 'blue-300',
-    icon: (
-      <BandcampIcon className="w-full h-full hover:text-blue-300 group-hover:text-blue-300" />
-    ),
-    text: 'Bandcamp (the best way to support! 🤑)',
+    title: '📣 M+T Review from Early Work Records 9/15/25',
+    url: 'https://www.earlyworkrecords.com/review/aloysius-the-band',
+    hoverColor: '[#1ed760]',
+    text: '📣 M+T Review from Early Work Records 9/15/25',
+    pullQuote:
+      '...the album as a whole is a moving document of a talented composer and singer pushing their impulses toward unpredictable results.',
   },
   {
     title: 'Interview with Mostly MN Music',
     url: 'https://mostlyminnesota.com/2025/06/23/5-questions-with-aloysius-the-band-on-upcoming-album-mortisetenon-releasing-july-4/',
     hoverColor: '[#1ed760]',
     text: 'Interview with Mostly MN Music 6/23/25',
+    pullQuote:
+      "...it's a techno-dreaming commentary and reaction on the interesting times in which we live. It makes you think and breathe and that's a nice combination these days.",
+  },
+  {
+    title: 'Bandcamp',
+    url: 'https://aloysius.bandcamp.com',
+    hoverColor: 'blue-300',
+    icon: (
+      <BandcampIcon className="w-full h-full hover:text-blue-300 group-hover:text-blue-300" />
+    ),
+    text: 'Bandcamp',
   },
   {
     title: 'Spotify',
@@ -37,16 +45,7 @@ const socialLinks = [
     icon: (
       <SpotifyIcon className="w-full h-full hover:text-[#1ed760] group-hover:text-[#1ed760]" />
     ),
-    text: 'AtB on Spotify',
-  },
-  {
-    title: 'Substack',
-    url: 'https://aloysiustheband.substack.com',
-    hoverColor: 'orange-500',
-    icon: (
-      <SubstackIcon className="w-full h-full hover:text-orange-500 group-hover:text-orange-500" />
-    ),
-    text: 'Writings and such',
+    text: 'Spotify',
   },
   {
     title: 'Instagram',
@@ -89,18 +88,26 @@ const IconLink = ({ url, title, icon }: SocialLink) => (
  * Expanded, linktree style link
  * @returns
  */
-const LinkCard = ({ url, title, icon = DEFAULT_ICON, text, hoverColor }: SocialLink) => {
+const LinkCard = ({
+  url,
+  title,
+  icon = DEFAULT_ICON,
+  text,
+  pullQuote, hoverColor,
+}: SocialLink) => {
   return (
     <a
       href={url}
       title={title}
       className="group p-3 border-4 border-white rounded-md flex flex-row gap-5 items-center min-w-[300px] md:transition-all hover:bg-slate-900 md:hover:-translate-y-1 md:hover:-translate-x-1 hover:border-blue-500"
     >
-      
-        <div className="flex items-center justify-center max-[360px]:w-8 max-[360px]:h-8 w-12 h-12 sm:w-16 sm:h-16">
-          {icon}
-        </div>
-      <p className="group-hover:text-blue-300">{text}</p>
+      <div className="flex items-center justify-center max-[360px]:w-8 max-[360px]:h-8 w-12 h-12 sm:w-16 sm:h-16">
+        {icon}
+      </div>
+      <div className='flex flex-col gap-1'>
+        <p className="group-hover:text-blue-300">{text}</p>
+        {pullQuote && <p className="text-sm italic">{pullQuote}</p>}
+      </div>
     </a>
   );
 };
